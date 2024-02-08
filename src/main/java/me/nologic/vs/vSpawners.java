c15ba0e48670854ae7a193916c8b1495c07affe4
BuildTools <unconfigured@null.spigotmc.org>
2024-02-08 13:34:54 +0600

    ...

--- src/main/java/me/nologic/vs/vSpawners.java
+++ src/main/java/me/nologic/vs/vSpawners.java
@@ -1,33 +1,9 @@
 package me.nologic.vs;
 
-import co.aikar.commands.PaperCommandManager;
 import lombok.Getter;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.CreatureSpawner;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.ExperienceOrb;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.SpawnerSpawnEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.world.ChunkUnloadEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BlockStateMeta;
-import org.bukkit.persistence.PersistentDataType;
+import co.aikar.commands.PaperCommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.ipvp.canvas.MenuFunctionListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public final class vSpawners extends JavaPlugin {
 
